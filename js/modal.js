b722f21d8fb1e262eda2e67162df256331dd2373
Daniela Correa <dacomu24@gmail.com>
2023-01-19 13:09:07 -0500

    It was create the function that listen the button 'create task' for now it only storage one task, it was also replace  getElementByClassName for getElementByID in the createTask var in line 6

--- js/modal.js
+++ js/modal.js
@@ -3,26 +3,35 @@ var modal = document.getElementById("modal");
 var openModalButton = document.getElementById("openModal");
 // Hace referencia al elemento <span> que tiene la X que cierra la ventana
 var closeButton = document.getElementsByClassName("close")[0];
-var createTask = document.getElementsByClassName("create-task");
-
-openModalButton.addEventListener("click",function() {
-    modal.style.display = "block";
-  });
-  // Si el usuario hace clic en la x, la ventana se cierra
-  closeButton.addEventListener("click",function() {
+var createTask = document.getElementById("create-task");
+var taskTittle = document.getElementById("task-tittle")
+var taskDescription = document.getElementById("task-description")
+openModalButton.addEventListener("click", function () {
+  modal.style.display = "block";
+});
+// Si el usuario hace clic en la x, la ventana se cierra
+closeButton.addEventListener("click", function () {
+  modal.style.display = "none";
+});
+// Si el usuario hace clic fuera de la ventana, se cierra.
+window.addEventListener("click", function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-  });
-  // Si el usuario hace clic fuera de la ventana, se cierra.
-  window.addEventListener("click",function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  });
+  }
+});
 
-  createTask.addEventListener('click', function(){
-    var taskTittle = document.getElementById("task-tittle").value
-var taskDescription = document.getElementById("task-description").value
-    modal.style.display="none"
-    localStorage.setItem("tasks",JSON.stringify({taskTittle:taskDescription}))
+createTask.addEventListener('click', function (e) {
+  e.preventDefault()
+  modal.style.display = "none"
+  localStorage.setItem("tasks", JSON.stringify(
+    {
+      0: {
+        "tittle": taskTittle.value,
+        "description": taskDescription.value
+      }
+    }))
+  console.log(taskTittle.value);
+  console.log(taskDescription.value);
 
-  })
+
+})
